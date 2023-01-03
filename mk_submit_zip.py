6b2e2fbbc0dc2481312ac34aa8f1f63916488247
t-nakatani <tomoya.12@icloud.com>
2023-01-03 02:12:09 +0000

    [ADD] some exp configs

--- mk_submit_zip.py
+++ mk_submit_zip.py
@@ -9,13 +9,14 @@ import sys
 args = sys.argv
 
 DIR_NAME = args[1]
+OUT_NAME = 'submit' if len(args) < 3 else args[2]
 img_paths = [os.path.join(DIR_NAME, fname) for fname in os.listdir(DIR_NAME)]
 for img_path in img_paths:
     img = cv2.imread(img_path)
     fname = os.path.basename(img_path)
     test_i = re.findall('test_[0-9]+', fname)[0]
     new_fname = f'{test_i}_answer.tif'
-    new_path = os.path.join('./submit', new_fname)
+    new_path = os.path.join('./results/submit', new_fname)
     cv2.imwrite(new_path, img)
 
-shutil.make_archive('submit', format='zip', root_dir='./submit')
+shutil.make_archive(OUT_NAME, format='zip', root_dir='./results/submit')
